b54f696d5958952cea9ac36ef92dc2505d578a0a
ananya singh <nakulghai@rediffmail.com>
2019-10-14 18:17:07 +0530

    pushing build

---
+++ build/precache-manifest.6bd53da542eafe0f511a15def11163b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0de39e7a0d5bfeab6585cc4dfef681de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3556328b16d8df9380b2",
+    "url": "/static/css/2.ae9e8b99.chunk.css"
+  },
+  {
+    "revision": "3556328b16d8df9380b2",
+    "url": "/static/js/2.fbc9b6e1.chunk.js"
+  },
+  {
+    "revision": "8c1dd4aefd06c3fffa3c",
+    "url": "/static/js/main.696504bc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
